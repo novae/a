810f8d1d0ec26c6b4681ee9b4e83bdbf5831897c
novae <novaegr@gmail.com>
2014-06-05 18:05:06 -0500

    v1 funcionando

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var cors = require("cors");
 var logger         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var gcm = require('node-gcm');
+
+var message = new gcm.Message();
+ 
+//API Server Key
+var sender = new gcm.Sender('AIzaSyC-aZfrwWIyqtxXI4lQR1pUJ7mOgJBY7YQ');
+
 
 
 mongoose.connect("mongodb://sourcode:sourcode@novus.modulusmongo.net:27017/e8Veharu");
@@ -30,6 +37,38 @@ app.get('/', function(req, res) {
     });
     
 });
+
+app.get('/send', function(req, res) {
+    Usuario.find(function(err,usuarios){
+    	if(err){
+    		console.log(err);
+    	}else{
+    		
+    		var registrationIds = [];
+			
+			for(i in usuarios){
+				registrationIds.push(usuarios[i].regId);
+			}
+			// Value the payload data to send...
+			message.addData('message',"dos \u270C Peace, Love \u2764 and PhoneGap \u2706!");
+			message.addData('title','ese sam' );
+			message.addData('msgcnt','3'); // Shows up in the notification in the status bar
+			message.addData('soundname','beep.wav'); //Sound to play upon notification receipt - put in the www folder in app
+			//message.collapseKey = 'demo';
+			//message.delayWhileIdle = true; //Default is false
+			message.timeToLive = 3000;// Duration in seconds to hold in GCM and retry before timing out. Default 4 weeks (2,419,200 seconds) if not specified.
+
+
+			sender.send(message, registrationIds, 4, function (result) {
+			    console.log(result);
+			});
+
+
+			res.send('Mensajes enviados');
+    	}
+    });
+});
+
 
 
 app.post('/login',cors(), function(req, res,next) {
